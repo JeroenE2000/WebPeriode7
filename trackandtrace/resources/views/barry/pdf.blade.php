ef81cf157376e3c1fd6a014ecbb6ba90c7cea7cd
Koen van Tilburg <71671751+Xoik@users.noreply.github.com>
2022-03-26 17:05:19 +0100

    pdffile
    dont let it be close to the kids

---
+++ trackandtrace/resources/views/barry/pdf.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Laravel 9 Generate PDF Example - ItSolutionStuff.com</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+    <h1>{{ $title }}</h1>
+    <p>{{ $date }}</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+    tempor incididunt ut labore et dolore magna aliqua.</p>
+
+    @foreach($labels as $label)
+    {{$label->TrackingNumber}}
+    <div class="mb-3">{!! DNS1D::getBarcodeHTML($label->TrackingNumber, 'UPCA') !!}</div>
+    @endforeach
+
+</body>
+</html>
