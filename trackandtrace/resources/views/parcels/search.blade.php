b79ad47201917a55fc75a5a46e73deb1e723f10c
Koen van Tilburg <71671751+Xoik@users.noreply.github.com>
2022-04-03 13:10:39 +0200

    home works
    ET PHONE HOME

--- trackandtrace/resources/views/parcels/search.blade.php
+++ trackandtrace/resources/views/parcels/search.blade.php
@@ -1,8 +1,11 @@
+@if(!Auth::check())
 @extends('layouts.app')
-
+@else
+@extends('layouts.admin')
+@endif
 @section('content')
     <section class="content">
-        <div class="col-12">
+        <div class="col-12 w-50 mx-auto">
             <div class="card">
                 <div class="card-header">
                     <h3 class="card-title">Find your package</h3>
@@ -10,14 +13,11 @@
                 <form action="{{ route('parcels.status') }}" method="POST">
                     @csrf
                     @method('put')
-                    <input type="number" name="TrackingNumber" class="form-control" required/></td>
+                    <input type="number" name="TrackingNumber" class="form-control w-75 mx-auto m-2" placeholder="Tracking Number..." required/></td>
                     <div class="d-grid mt-3">
-                      <input type="submit" class="btn btn-primary" value="Submit">
+                      <input type="submit" class="btn btn-success w-20 mx-auto m-2" value="Check">
                     </div>
                 </form>
-                {{-- <div class="pull-right mt-3">
-                    <a class="btn btn-primary" href="{{ route('parcels.status') }}"> Back</a>
-                </div> --}}
             </div>
         </div>
     </section>
--- trackandtrace/resources/views/parcels/show.blade.php
+++ trackandtrace/resources/views/parcels/show.blade.php
@@ -1,15 +1,33 @@
+@if(!Auth::check())
 @extends('layouts.app')
-
+@else
+@extends('layouts.admin')
+@endif
 @section('content')
     <section class="content">
-        <div class="col-12">
+        <div class="col-12 w-50 mx-auto">
             <div class="card">
                 <div class="card-header">
                     <h3 class="card-title">Your package's status</h3>
                 </div>
                @foreach($parcel as $p)
-                    {{$p->parcel_status->state}}
+                    <div class="w-75 mx-auto m-2">
+                        <div class="card">
+                            <div class="card-header">
+                                <h3 class="card-title">{{$p->parcel_label->Package_name}}</h3>
+                            </div>
+                            <div class="card-body">
+                                {{$p->parcel_label->Dimensions}} cm<br>
+                                {{$p->parcel_label->Weight}} KG<br>
+                                Address: {{$p->parcel_label->Address_Reciever}}<br>
+                                <input class="form-control" value="{{$p->parcel_status->state}}" disabled>
+                            </div>
+                        </div>
+                    </div>
                @endforeach
+               <div class="pull-right mt-3 mx-auto m-2">
+                <a class="btn btn-primary" href="{{ route('parcels.search') }}"> Back</a>
+            </div>
             </div>
         </div>
     </section>
